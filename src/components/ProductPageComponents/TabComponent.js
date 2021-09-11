ae976033d06411758f56bb3d4b0ecf914b779338
anuj2110 <atrehan789@gmail.com>
2021-09-11 19:16:25 +0530

    started changing TabComponent

--- src/components/ProductPageComponents/TabComponent.js
+++ src/components/ProductPageComponents/TabComponent.js
@@ -1,32 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Tab, Tabs } from "react-bootstrap";
-import ProductList from "./ProductList";
-import CategoryList from "./CategoryList";
-import { useContext } from "react";
-import { AppContext } from "../../context/Context";
 
 
 const TabComponent = () => {
-  const { appState } = useContext(AppContext);
-
-  useEffect(() => {}, [appState.products]);
 
   const [key, setKey] = useState("Products");
   return (
-    <div className="container mx-3 my-3">
+    <div className="container mx-2 my-2">
       <Tabs
         id="controlled-tab"
         activeKey={key}
         onSelect={(k) => setKey(k)}
         className="mb-3 mx-3">
         <Tab eventKey="Products" title="Products">
-          <ProductList
-            products={appState.products}
-            categories={appState.categories}
-          />
+          
         </Tab>
         <Tab eventKey="Categories" title="Categories">
-          <CategoryList categories={appState.categories} />
+          
         </Tab>
       </Tabs>
     </div>
