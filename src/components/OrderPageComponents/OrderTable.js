5a5a4f0eaf2e44c93293492f9e9d21ee27f97415
anuj2110 <atrehan789@gmail.com>
2021-09-27 15:00:26 +0530

    added name prop

--- src/components/OrderPageComponents/OrderTable.js
+++ src/components/OrderPageComponents/OrderTable.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
+import { firestore } from "../../Firebase";
 import TableRow from "./TableRow";
 
-
-const OrderTable = ({ orders, ordersPerPage, setOrdersPerPage }) => {
+const OrderTable = ({ orders, ordersPerPage, setOrdersPerPage,name }) => {
   const recordsList = [10, 15, 20];
+
+ 
   return (
     <div className="m-4">
       <div className="card shadow mb-4">
         <div className="card-header py-3">
-          <h6 className="m-0 font-weight-bold text-primary">Orders</h6>
+          <h6 className="m-0 font-weight-bold text-primary">{name}</h6>
         </div>
         <div className="card-body">
           Per Page Records:
