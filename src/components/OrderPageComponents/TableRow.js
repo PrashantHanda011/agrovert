a549bfaf6f0a1240246f233ac0bcbea4f5b6b485
anuj2110 <atrehan789@gmail.com>
2021-09-27 15:00:55 +0530

    added changes for confirm modal

--- src/components/OrderPageComponents/TableRow.js
+++ src/components/OrderPageComponents/TableRow.js
@@ -3,11 +3,16 @@ import { Badge, Button } from "react-bootstrap";
 import FullOrderPage from "./FullOrderPage";
 import OrderModule from '../../modules/orderModule';
 import CustomerModule from "../../modules/customerModule";
+import { firestore } from "../../Firebase";
+import ConfirmModal from "./ConfirmModal";
 
 const TableRow = ({ Order, index }) => {
   const [order_, setOrder] = useState(Order);
-  const [user, setUser] = useState(null);
+
   const [open, setOpen] = useState(false);
+  const [openConfirm, setOpenConfirm] = useState(false);
+  const [users,setUsers] = useState(null)
+  const [actionType,setActionType] = useState("")
   const orderModule = new OrderModule();
   const customerModule =  new CustomerModule()
 
@@ -16,6 +21,14 @@ const TableRow = ({ Order, index }) => {
   };
   const handleClose = () => {
     setOpen(false);
+  };
+
+  
+  const handleOpenConfirm = () => {
+    setOpenConfirm(true);
+  };
+  const handleCloseConfirm = () => {
+    setOpenConfirm(false);
   };
   const approveOrder = () => {
     setOrder({ ...order_, status: "DELIVERED" });
@@ -29,24 +42,28 @@ const TableRow = ({ Order, index }) => {
     orderModule.updateOrderStatus(order_.id, "CANCELLED");
   };
   useEffect(() => {
-    const getUser = async (id) => {
-      const data = await orderModule.getUserFromUserId(id);
-      setUser(data);
+    const getUsers = async () => {
+      const users_ = await firestore.collection("users").get()
+      let data = {}
+      users_.docs.forEach(doc=>{
+        data[doc.id] = doc.data()
+      })
+      setUsers(data);
     };
-    getUser(order_.user_id);
-  }, []);
+    getUsers()
+  }, [Order]);
 
   useEffect(() => {
     setOrder(Order);
   }, [Order]);
   return (
     <>
-      {user && (
+      {users && (
         <tr className="align-items-center">
           <td>{index + 1}</td>
           <td>{order_.timestamp.toDate().toDateString()}</td>
-          <td>{user.name}</td>
-          <td>{user.phone_number}</td>
+          <td>{users[order_.user_id].name}</td>
+          <td>{users[order_.user_id].phone_number}</td>
           <td>₹{order_.amount.toFixed(2)}</td>
           <td>
             <Badge
@@ -74,7 +91,9 @@ const TableRow = ({ Order, index }) => {
               className="btn btn-sm btn-success"
               disabled={order_.status === "DELIVERED" ? true : false}
               onClick={() => {
-                approveOrder();
+                handleOpenConfirm()
+                setActionType("approve")
+              
               }}>
               Approve
             </button>
@@ -84,7 +103,9 @@ const TableRow = ({ Order, index }) => {
               className="btn btn-sm btn-danger"
               disabled={order_.status === "CANCELLED" ? true : false}
               onClick={() => {
-                cancelOrder();
+                handleOpenConfirm()
+                setActionType("cancel")
+                
               }}>
               Cancel
             </button>
@@ -94,11 +115,28 @@ const TableRow = ({ Order, index }) => {
       {open && (
         <FullOrderPage
           order={order_}
-          user={user}
+          user={users[order_.user_id]}
           show={open}
           handleClose={handleClose}
         />
       )}
+      {openConfirm && 
+      actionType==="approve"?
+      (
+        <ConfirmModal
+          action={approveOrder}
+          show={openConfirm}
+          handleClose={handleCloseConfirm}
+        />
+      ):
+      actionType==="cancel"?
+      (
+        <ConfirmModal
+          action={cancelOrder}
+          show={openConfirm}
+          handleClose={handleCloseConfirm}
+        />
+      ):<></>}
     </>
   );
 };
